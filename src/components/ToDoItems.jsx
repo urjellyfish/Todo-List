b9d4c94c58bbd835a6779074a6d6ca65679ea91f
urjellyfish <annaphan0511@gmail.com>
2025-08-06 17:26:17 +0700

    feat: add, delete and mark complete task

--- src/components/ToDoItems.jsx
+++ src/components/ToDoItems.jsx
@@ -2,13 +2,15 @@ import tick from '../assets/tick.png'
 import not_tick from '../assets/not_tick.png'
 import delete_icon from '../assets/delete.png'
 
-const ToDoItems = () => {
+const ToDoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
   return (
     <div className="flex items-center my-3 gap-2">
-      <div className='flex flex-1 items-center'>
-        <img src={tick} alt="tick"/>
-        <p>Learning Coding</p>
+      <div onClick={()=>{toggle(id)}} className='flex flex-1 items-center'>
+        <img src={isComplete ? tick : not_tick} alt="tick"/>
+        <p className={`text-slate-700 ml-4 text-[17px] ${isComplete ? "line-through" : ""}`}>{text}</p>
       </div>
+
+      <img onClick={()=>{deleteTodo(id)}} className='w-5 cursor-pointer' src={delete_icon} alt="delete" />
     </div>
   )
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import ToDoItems from './ToDoItems'
 
 const Todo = () => {
+
+    const [todoList, setTodoList] = useState(localStorage.getItem("todos")?
+    JSON.parse(localStorage.getItem("todos")):[]);
+    const inputRef = useRef()
+    const add = () => {
+        const inputText = inputRef.current.value.trim();
+
+        if(inputText === ""){
+            return null;
+        }
+
+        console.log(inputText);
+        const newTodo = {
+            id: Date.now(),
+            text: inputText,
+            isComplete: false
+        }
+        setTodoList((prev)=> [...prev, newTodo]);
+        inputRef.current.value = "";
+    }
+
+    const deleteTodo = (id) => {
+        setTodoList((prevTodo) =>{
+            return prevTodo.filter((todo) => todo.id !== id)
+        })
+    }
+
+    const toggle = (id) =>{
+        setTodoList((prvTodos) =>{
+            return prvTodos.map((todo) =>{
+                if(todo.id === id){
+                    return {...todo, isComplete: !todo.isComplete}
+                }
+                return todo;
+            })
+        })
+    }
+
+    useEffect(()=>{
+        localStorage.setItem("todos", JSON.stringify(todoList))
+    },[todoList])
+
   return (
     <div className="bg-pink-100 place-self-center w-11/12 max-w-md
     flex flex-col p-7 min-h-[550px] rounded-xl">
@@ -14,15 +57,18 @@ const Todo = () => {
 
         {/*Input box*/}
         <div className='flex items-center my-7 bg-pink-50 rounded-full'>
-            <input className='bg-transparent border-0 outline-none
+            <input ref={inputRef} className='bg-transparent border-0 outline-none
             flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type='text' placeholder='Add your text'></input>
-            <button className='border-none rounded-full bg-pink-300 w-32 h-14 text-white text-lg font-medium cursor-pointer'>
+            <button onClick={add} className='border-none rounded-full bg-pink-300 w-32 h-14 text-white text-lg font-medium cursor-pointer hover:bg-pink-400'>
             Add</button>
         </div>
 
         {/*todo list*/}
         <div>
-            <ToDoItems/>
+        {todoList.map((item, index) =>{
+            return <ToDoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete}
+            deleteTodo={deleteTodo} toggle ={toggle}></ToDoItems>
+        })}
         </div>
     </div>
   )
